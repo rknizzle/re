e8e19bf773da5f9f7bfba111bc43553294aaac46
rknizzle <ryan@autotiv.com>
2020-07-03 16:09:59 -0400

    Added a function that attemps to clear the terminal screen
            - to be ran between command runs

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"os/exec"
+	"runtime"
 )
 
 // CLI entrypoint
@@ -14,4 +17,34 @@ func main() {
 		log.Fatalf(err.Error())
 	}
 	return
+}
+
+/*
+	Attempt to clear the terminal screen depending on the OS. Print a warning
+	message if the system is not supported
+	NOTE: only tested on macOS
+*/
+var clearScreen = func() {
+	switch system := runtime.GOOS; system {
+	case "darwin":
+		func() {
+			cmd := exec.Command("clear")
+			cmd.Stdout = os.Stdout
+			cmd.Run()
+		}()
+	case "linux":
+		func() {
+			cmd := exec.Command("clear")
+			cmd.Stdout = os.Stdout
+			cmd.Run()
+		}()
+	case "windows":
+		func() {
+			cmd := exec.Command("cmd", "/c", "cls")
+			cmd.Stdout = os.Stdout
+			cmd.Run()
+		}()
+	default:
+		fmt.Println("Platform unsupported! Could not clear the screen")
+	}
 }
