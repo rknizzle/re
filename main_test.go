fa74602edc068f470ef3e3ec87d701ce45657ff4
rknizzle <ryan@autotiv.com>
2020-07-05 17:22:40 -0400

    add a test to verify that the command runs a 2nd time...
            ... if an event (AKA a file change) is triggered

--- main_test.go
+++ main_test.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"github.com/fsnotify/fsnotify"
 	"os"
 	"os/exec"
 	"testing"
+	"time"
 )
 
 func TestMainThrowsAnErrorWhenGivenNoCommandArguments(t *testing.T) {
@@ -48,5 +50,42 @@ func TestCommandRunsOnStartup(t *testing.T) {
 		t.Errorf("initializeCmd was not called")
 	} else if runCount != 1 {
 		t.Errorf("Expected initializeCmd to be called once but it was called %d times", runCount)
+	}
+}
+
+func TestEventTriggersCommandToReRun(t *testing.T) {
+	clearScreen = func() {}
+	var runCount int = 0
+	before := initializeCmd
+
+	// replace the intializeCmd fxn with a fxn that increments a called counter
+	// so that the test can verify if initializeCmd was called
+	initializeCmd = func([]string) *exec.Cmd {
+		runCount++
+		cmd := exec.Command("echo", "")
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		return cmd
+	}
+
+	// un-mock initializeCmd after the test
+	defer func() {
+		initializeCmd = before
+	}()
+
+	done := make(chan bool)
+	// start the run function
+	go run([]string{"blank", "echo", ""}, done)
+
+	// QUICKFIX: for some reason this test hangs if this sleep isnt here. Im
+	// guessing it is because I need to wait for the deployWatchers function to
+	// finish before any watcher events can be triggered
+	time.Sleep(500 * time.Millisecond)
+	// send a watcher event as a way to mock a file change
+	watcher.Events <- fsnotify.Event{}
+
+	done <- true
+	if runCount != 2 {
+		t.Errorf("Command should run twice but it ran %d times", runCount)
 	}
 }
